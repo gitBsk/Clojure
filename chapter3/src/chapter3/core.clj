1a1c075a0b1b7adffd2a3429761d11fda76c1279
Balaji S <bsanthana@altimetrik.com>
2019-03-01 17:02:05 +0530

    Adding Solution to Question 4

--- chapter3/src/chapter3/core.clj
+++ chapter3/src/chapter3/core.clj
@@ -34,13 +34,37 @@
 )
 
 ;Alternate solution [shorter code] for question2
-(#(println (str "the new number is: " (+ % 100))) 20) 
+;(#(println (str "the new number is: " (+ % 100))) 20) 
+
+(defn dec-maker  
+"Create a custom decrementor" 
+[dec-by]
+(println "===================== Question 3 ====================" )
+#(- % dec-by)
+)
+
+(defn mapset 
+"Functions like a map but returns a collection"
+[func args]
+(println "===================== Question 4 ====================" )
+(println (str "The converted collection is :"(set (map func args))))
+)
 
 
 
+;Remove ; to execute questions
+;================================
+
+;Call for Question 1
 ;(question1)
+
+;Call for Question 2
 ;(question2 56)
 
+;Call for Question 3
+;(def dec9 (dec-maker 9))
+;(println (str "the number is " (dec9 10)))
 
-
+;Call for Question 4
+;(mapset inc [1 2 4 5 2 1 5 4 3 4 5 5 1 2])
 
