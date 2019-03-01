83488481dee1d980c0c81a00eb34a834a54e4172
Balaji S <bsanthana@altimetrik.com>
2019-03-01 10:16:26 +0530

    Chapter 1 Ex file

---
+++ bala-clojure-noob/src/bala_clojure_noob/core.clj
@@ -0,0 +1,7 @@
+(ns bala-clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "My First Clojure Program"))
---
+++ bala-clojure-noob/test/bala_clojure_noob/core_test.clj
@@ -0,0 +1,7 @@
+(ns bala-clojure-noob.core-test
+  (:require [clojure.test :refer :all]
+            [bala-clojure-noob.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
