2bc2e5f10e37f9ca13df35c2f27f7b0c81b9763c
Balaji Santhana <balaji.santhana@blr-fvfxn02shv2d.fundingcircle.global>
2019-03-12 16:26:46 +0530

    solution for ex3

--- chapter5/src/chapter5/core.clj
+++ chapter5/src/chapter5/core.clj
@@ -28,3 +28,25 @@
 (fn1 (reduce fn2 args))))
 
 ;Sample O/P => (println ((comp4 inc +) 2 3))
+
+;DataMap to be tested
+(def users {:name "James" :age 26})
+
+;Solution to Ex 3
+(defn bala-assoc
+"Implement assoc in function"
+[m k v]
+(assoc m k v)
+)
+
+(bala-assoc users :name "Superman")
+
+(def users2 [{:name "James" :age 26}  {:name "John" :age 43}])
+
+(defn bala-assoc-in
+"Implement assoc in function"
+[m [k & kws] v]
+(bala-assoc m k (bala-assoc-in (get m k) kws v))
+)
+
+(bala-assoc-in users2 [1 :name] "superman")
